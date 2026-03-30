da5f520616b33e6822b48169c0c8c221d69e82c2
rhwlgns4386 <rhwlgns4386@naver.com>
2026-03-31 01:20:00 +0900

    test : 각체 경로 초기화 코드 수정

--- test/helper.test.ts
+++ test/helper.test.ts
@@ -3,7 +3,7 @@ import ffprobePath from 'ffprobe-static';
 import { getDuration } from '../src/helper.ts';
 import assert from 'assert';
 
-process.env.FFPROBE_PATH = ffprobePath.path;
+process.env.FFPROBE_PATH = process.env.FFPROBE_PATH ?? ffprobePath.path;
 
 test('시간_조회', async () => {
     const time: number = await getDuration('https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4');
